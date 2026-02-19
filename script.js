a98c7b015b6c149e547c3d64ec43844e2222bc86
Bryan C <bryan.cast@outlook.com>
2026-02-19 14:07:52 +1030

    value turning red when pressing minus button and is == 0;

--- script.js
+++ script.js
@@ -8,28 +8,33 @@ const countDisplay = document.getElementById('count');
 // Store current count
 let count = 0; // defaults to 0
 
+
 // Increase button
 // Add event listener to increase button
 increaseBtn.addEventListener('click', () => {
     count++; // increment count by 1
-    countDisplay.textContent = count; // update the displayed count
+    if(count > 0) {
+        countDisplay.style.color = 'black'; // reset color to black if it was red
+        countDisplay.textContent = count; // update the displayed count
+    }
 });
 
 // Decrease button
 // Add event listener to decrease button
 decreaseBtn.addEventListener('click', () => {
-    if(count <= 0) {
-        return; // if count is 0 or less, do nothing and exit the function
-    }
-    else {
+    countDisplay.textContent = count; // update the displayed count
+    if(count > 0) {
         count--; // decrement count by 1
+        }
+    if(count <= 0) {
+        countDisplay.style.color = 'red';
         countDisplay.textContent = count; // update the displayed count
     }
 });
 
+
 // Reset button
 // Add event listener to reset button
-
 resetBtn.addEventListener('click', () => {
     count = 0; // reset count to 0
     countDisplay.textContent = count; // update the displayed count
